519d39615e4f686a716a2afd17bfdf5fd3e788c5
= <fhmsholihin09@gmail.com>
2018-01-26 13:38:57 +0800

    Komit sholihin kedua

--- resources/views/email/register.blade.php
+++ resources/views/email/register.blade.php
@@ -2,10 +2,10 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Register 4ECOM Projek UAS 5C</title>
+    <title>Register FShop Projek UAS Visual 3</title>
   </head>
   <body>
-    <h1>Register 4ECOM Projek UAS 5C</h1>
+    <h1>Register FShop Projek UAS Visual 3</h1>
     <a href="http://localhost:8000/verify/{{$user->token}}/{{$user->id}}">Klik Untuk Mengaktifkan Akun</a>
   </body>
 </html>
